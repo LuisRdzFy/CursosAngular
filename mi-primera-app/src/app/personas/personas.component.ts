e582641760d86307a9300fdd578609508a90635a
LuisRdzFy <luis.rodriguez@fyware.com>
2022-12-01 10:44:06 -0600

    Condicional If

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -6,5 +6,18 @@ import { Component } from "@angular/core";
     styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent{
+    deshabilitar = false;
+    mensaje = "No se ha agregado ninguna persona";
+    titulo = "Ingeniero";
+    mostrar = false;
 
+    agregarPersona(){
+        this.mensaje = "Persona agregada"
+        this.mostrar = true;
+    }
+    // modificarTitulo(event: Event){
+    //     this.titulo = (<HTMLInputElement>event.target).value
+
+
+    // }
 }
