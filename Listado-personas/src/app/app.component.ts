b89fb585b52b3843615b65a0d8da1f9837f3ed42
LuisRdzFy <luis.rodriguez@fyware.com>
2022-12-02 09:19:07 -0600

    Proyecto ngFor Listado de Personas

--- Listado-personas/src/app/app.component.ts
+++ Listado-personas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona_model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Listado-personas';
+  title = 'Listado de personas';
+
+  personas: Persona[] = [
+    new Persona('Luis', 'Rodriguez'),
+    new Persona('Eunice', 'Torres'),
+    new Persona('Prueba', 'Lorem'),
+  ];
+  nombreInput: string ='';
+  apellidoInput: string ='';
+
+  agregarPersona():void{
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
+
+
 }
--- Listado-personas/src/app/app.module.ts
+++ Listado-personas/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
